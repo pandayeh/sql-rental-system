69e2378a575896bbe09a2d42bb24b7076e939fb7
pandayeh <wilsonpyeh@gmail.com>
2014-07-23 16:52:40 -0500

    SQLite Reference, Inventory Window

--- Rental/Rental/Inventory.xaml.cs
+++ Rental/Rental/Inventory.xaml.cs
@@ -11,26 +11,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Threading;
 
 namespace Rental
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Inventory.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Inventory : Window
     {
-        public MainWindow()
+        public Inventory()
         {
             InitializeComponent();
-            DispatcherTimer timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
-            {
-                this.clock.Text = DateTime.Now.ToString();
-            }, this.Dispatcher);
-
-
         }
     }
 }
